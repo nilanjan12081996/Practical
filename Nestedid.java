530b1d008f4554c756ebaf1bc8e2367747aaee4e
nilanjan12081996 <107671025+nilanjan12081996@users.noreply.github.com>
2022-06-27 12:41:04 +0530

    Update Nestedid.java

--- Nestedid.java
+++ Nestedid.java
@@ -6,11 +6,11 @@ public class Nestedid {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		String address="kolkata, India";
-		if(address.endsWith("India"))
+		if(address.endsWith("India"))//check is wheather the string is endswith india or not
 		{
-			if(address.contains("Dunlop"))
+			if(address.contains("Dunlop"))//check is wheather the string is contian dunlop or not
 				System.out.println("Your city is dunlop");
-			else if(address.contains("Newtown"))
+			else if(address.contains("Newtown"))//check is wheather the string is contian newtown or not
 				System.out.println("your city is newtown");
 			else
 				System.out.println(address.split(",")[0]);
