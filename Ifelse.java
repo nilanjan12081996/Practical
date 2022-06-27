946f601ad0a88706d4f4daad7dec4b52c46e4d35
nilanjan12081996 <107671025+nilanjan12081996@users.noreply.github.com>
2022-06-27 12:34:43 +0530

    Update Ifelse.java

--- Ifelse.java
+++ Ifelse.java
@@ -7,7 +7,7 @@ public class Ifelse {
 		// TODO Auto-generated method stub
 		int x=5;
 		int y=3;
-		if(x+y<10)
+		if(x+y<10)// Check the codition is there x+y is less or not
 			System.out.print("x+y is less than 10");
 		else
 			System.out.println("x+y is greater than 10");
