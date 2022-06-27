1db491ad06e8c3611aeb51f043a689f73603981e
nilanjan12081996 <107671025+nilanjan12081996@users.noreply.github.com>
2022-06-27 12:42:06 +0530

    Update Simpleif.java

--- Simpleif.java
+++ Simpleif.java
@@ -5,9 +5,9 @@ public class Simpleif {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int x=5;
-		int y=3;
-		if(x+y<10)
+		int x=5; //initialize x as 5
+		int y=3; //initialize x as 3
+		if(x+y<10) //Check condition for x+y is less than 10 or not
 			System.out.print("X+y is less than 10");
 
 	}
