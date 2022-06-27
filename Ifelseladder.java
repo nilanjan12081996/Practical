0d2ab23350a141143ce2cd1504157e10f174b1e5
nilanjan12081996 <107671025+nilanjan12081996@users.noreply.github.com>
2022-06-27 12:33:40 +0530

    Update Ifelseladder.java

--- Ifelseladder.java
+++ Ifelseladder.java
@@ -5,12 +5,12 @@ public class Ifelseladder {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String name="Riya";
-		if (name=="Meena")
+		String name="Riya"; //initializing the string
+		if (name=="Meena") //checking condition is there string is Meena or not
 			System.out.println("name is Meena");
-		else if(name=="Neha")
+		else if(name=="Neha") //checking condition is there string is Neha or not
 			System.out.println("name is Neha");
-		else if(name=="Amit")
+		else if(name=="Amit") //checking condition is there string is Amit or not
 			System.out.println("name is Amit");
 		else
 			System.out.println(name);
